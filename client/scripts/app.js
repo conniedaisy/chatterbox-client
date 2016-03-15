eebf8533082067632c498ae528ff2031db849d19
conniedaisy <conniedcheung@gmail.com>
2016-03-14 17:26:19 -0700

    Add clear messages button

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,125 +1,139 @@
 // YOUR CODE HERE:
+$(document).ready(function() {
+  var app = {
+    server: 'https://api.parse.com/1/classes/messages',
+    results: []
 
-var app = {
-  server: 'https://api.parse.com/1/classes/messages',
-  results: []
+  };
+    
+  app.displayMessages = function(data) {
 
-};
-  
-app.displayMessages = function(data) {
+    var $chats = $('#chats');
+    var context = this;
 
-  var $chats = $('#chats');
-  var context = this;
+    data.forEach(function(element) { //WHY IS UNDERSCORE NOT WORKING?
 
-  data.forEach(function(element) { //WHY IS UNDERSCORE NOT WORKING?
+      var username = element.username;
+      var text = element.text;
+      var createdAt = element.createdAt;
 
-    var userName = element.userName;
-    var text = element.text;
-    var createdAt = element.createdAt;
+      var safeText = context.escapeForHtml(text);
+      var safeCreatedAt = context.escapeForHtml(createdAt);
+      var safeUsername = context.escapeForHtml(username);
 
-    var safeText = context.escapeForHtml(text);
-
-    $chats.append('<div class="message"></div>')
-      .append('<div>' + element.username + '</div>')
-      .append('<div>' + safeText + '</div>')
-      .append('<div>' + element.createdAt + '</div>');
+      $chats.append('<div class="message"></div>')
+        .append('<div>' + safeUsername + '</div>')
+        .append('<div>' + safeText + '</div>')
+        .append('<div>' + safeCreatedAt + '</div>');
 
 
-  });
+    });
 
-};
+  };
 
-//TRY REGEX LATER
-app.escapeForHtml = function(string) {
+  //TRY REGEX LATER
+  app.escapeForHtml = function(string) {
 
-  var safeString = '';
-  string = string || '';
+    var safeString = '';
+    string = string || '';
 
-  for (var i = 0; i < string.length; i++) {
-    if (string[i] === '&') {
-      safeString += '&amp;';
-    } else if (string[i] === '<') {
-      safeString += '&lt;';
-    } else if (string[i] === '>') {
-      safeString += '&gt;';
-    } else if (string[i] === '"') {
-      safeString += '&quot;';
-    } else if (string[i] === '\'') {
-      safeString += '&#x27;';
-    } else if (string[i] === '/') {
-      safeString += '&#x2F;';
-    } else {
-      safeString += string[i];
+    for (var i = 0; i < string.length; i++) {
+      if (string[i] === '&') {
+        safeString += '&amp;';
+      } else if (string[i] === '<') {
+        safeString += '&lt;';
+      } else if (string[i] === '>') {
+        safeString += '&gt;';
+      } else if (string[i] === '"') {
+        safeString += '&quot;';
+      } else if (string[i] === '\'') {
+        safeString += '&#x27;';
+      } else if (string[i] === '/') {
+        safeString += '&#x2F;';
+      } else {
+        safeString += string[i];
+      }
     }
-  }
 
-  return safeString;
-};
+    return safeString;
+  };
 
-app.init = function() {};
+  app.init = function() {
+    $('.clearButton').on('click', app.clearMessages);
 
-app.send = function(message) {
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/messages/',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
+  };
 
-
-
-};
-
-
-app.fetch = function() {
-
-  var context = this;
-
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'GET',
-    data: '',
-    contentType: 'application/json',
-    success: function (data) {
-      this.results = data.results;
-
-     // $('#chats').append(JSON.stringify(data.results.createdAt));
-      context.displayMessages(data.results);
-
-      console.log('chatterbox: Message received');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to receive message', data);
-    }
-  });
-
-};
-
-app.clearMessages = function() {
-
-};
-
-app.addMessage = function(message) {
-
-};
+  app.send = function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages/',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
 
 
 
-var message = {
-  username: 'Mel Brooks',
-  text: 'It\'s good to be the king',
-  roomname: 'lobby'
-};
+  };
 
-app.fetch();
-// app.send(message);
+
+  app.fetch = function() {
+
+    var context = this;
+
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'GET',
+      data: '',
+      contentType: 'application/json',
+      success: function (data) {
+        this.results = data.results;
+
+       // $('#chats').append(JSON.stringify(data.results.createdAt));
+        context.displayMessages(data.results);
+
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to receive message', data);
+      }
+    });
+
+  };
+
+  app.clearMessages = function() {
+    // get the chat html element using jQuery
+    console.log('in clearMessages function');
+    var $chats = $('#chats');
+    $chats.empty();
+    // empty it
+
+  };
+
+  app.addMessage = function(message) {
+
+  };
+
+
+
+  var message = {
+    username: 'Mel Brooks',
+    text: 'It\'s good to be the king',
+    roomname: 'lobby'
+  };
+
+  app.init();
+
+  app.fetch();
+  // app.send(message);
+
+});
